e8836e40a6a0d80216510b0766ebd64648527f03
Ngeorge <142554602+nicolergeorge@users.noreply.github.com>
2024-09-28 06:56:28 -0400

    Update script.js

--- script.js
+++ script.js
@@ -12,5 +12,17 @@ searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 //API Info
 
-apiKey = "b5ff830c8o2f986ffadf3d6aab41t4c5"
-apiURL = "https://api.shecodes.io/weather/v1/current?query={city}&key={apiKey}"
+let apiKey = "b5ff830c8o2f986ffadf3d6aab41t4c5"
+let apiURL = "https://api.shecodes.io/weather/v1/current?query={city}&key={apiKey}&units=imperial"
+axios.get(apiURL).then(refreshWeather);
+
+//refresh weather function
+
+function refreshWeather(response) {
+    let temperatureElement = document.querySelector("#temperature");
+    let temperature = response.data.temperature.current;
+    let cityElement = document.querySelector("#city")
+
+    cityElement.innerHTML = response.data.city;
+    temperature.Element.innerHTML - Math.round(temperature);
+}
