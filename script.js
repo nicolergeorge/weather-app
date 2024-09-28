9459de597c6cd10fb8f185ef2f9e633a088432b0
Ngeorge <142554602+nicolergeorge@users.noreply.github.com>
2024-09-27 20:03:44 -0400

    API AND UNIT

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//search city name function
+
 function handleSearchSubmit(event) {
     event.preventDefault();
     let searchInput = document.querySelector("#search-form-input");
@@ -7,3 +9,8 @@ function handleSearchSubmit(event) {
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+//API Info
+
+apiKey = "b5ff830c8o2f986ffadf3d6aab41t4c5"
+apiURL = "https://api.shecodes.io/weather/v1/current?query={city}&key={apiKey}"
